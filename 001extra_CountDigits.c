6786be8eac2dbccb4fe4f7f06d8a46d7f74d337d
Anant643 <anantsharma6034@gmail.com>
2026-02-26 15:31:07 +0530

    Create 001extra_CountDigits.c

---
+++ 001extra_CountDigits.c
@@ -0,0 +1,24 @@
+// WAP to count digits in a number:-
+
+#include <stdio.h>
+int main()
+{
+    int n, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n == 0)
+    {
+        count = 1;
+    }
+    else
+    {
+        while (n != 0)
+        {
+            count = n / 10;
+            count++;
+        }
+    }
+    printf("The number of digits in %d is %d.", n, count);
+    return 0;
+}
