08b3a26e49c08848a5388e4e53982189b209b9ce
Anant643 <anantsharma6034@gmail.com>
2026-03-12 22:07:15 +0530

    Implement input validation for upper limit
    Added input validation for positive numbers.

--- 14_Find_EvenSum_OddSum.c
+++ 14_Find_EvenSum_OddSum.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
+
 int main()
 {
-    int n;
+    int n, i;
     int evenSum = 0, oddSum = 0;
+
     printf("Enter upper limit: ");
     scanf("%d", &n);
 
-    for (int i = 1; i <= n; i++)
+    if(n < 1)
     {
-        if (i % 2 == 0)
-        {
-            evenSum += i;
-        }
-        else
-        {
-            oddSum += i;
-        }
+        printf("Enter a positive number.");
+        return 0;
     }
+
+    for(i = 1; i <= n; i++)
+    {
+        if(i % 2 == 0)
+            evenSum += i;
+        else
+            oddSum += i;
+    }
+
     printf("Sum of even numbers = %d\n", evenSum);
     printf("Sum of odd numbers = %d\n", oddSum);
 
