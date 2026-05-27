956be549bb28a2ec55cb89068e25c5d2925225be
Anant643 <anantsharma6034@gmail.com>
2026-05-27 22:54:06 +0530

    Update 001_CountDigits.c

--- 001_CountDigits.c
+++ 001_CountDigits.c
@@ -1,24 +1,29 @@
 // WAP to count digits in a number:-
-
 #include <stdio.h>
+
 int main()
 {
-    int n, count = 0;
+    int n, original, count = 0;
 
     printf("Enter a number: ");
     scanf("%d", &n);
-    if (n == 0)
+
+    original = n;
+
+    if(n == 0)
     {
         count = 1;
     }
     else
     {
-        while (n != 0)
+        while(n != 0)
         {
-            count = n / 10;
+            n = n / 10;
             count++;
         }
     }
-    printf("The number of digits in %d is %d.", n, count);
+
+    printf("The number of digits in %d is %d.", original, count);
+
     return 0;
 }
